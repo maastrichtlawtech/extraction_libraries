4f43d59f7e0829c4308f798c8ac981ff67240fb7
Piotr Lewandowski <pmlewandowski@gmail.com>
2023-04-10 13:12:50 +0200

    rechtspraak lib update on pypi

--- rechtspraak/rechtspraak_extractor/testing_file.py
+++ rechtspraak/rechtspraak_extractor/testing_file.py
@@ -1,5 +1,5 @@
 from rechtspraak import *
 from rechtspraak_metadata import *
 df = get_rechtspraak(max_ecli=100,sd='2022-01-01',save_file='y')
-get_rechtspraak_metadata(save_file='y')
-b=2
+get_rechtspraak_metadata(save_file='y',dataframe=df)
+pass
