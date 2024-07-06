66c5ad1914e97c249f5047b989b313a6f89bc5c0
venvis <127123047+venvis@users.noreply.github.com>
2024-07-06 17:34:11 +0200

    Update  to version 1.1.1

--- cellar/setup.py
+++ cellar/setup.py
@@ -10,7 +10,7 @@ long_descr = p.read_text()
 setup(
     name='cellar_extractor',
     packages=find_packages(include=['cellar_extractor', 'cellar_extractor.operative_extractions']),
-    version='1.1.0',
+    version='1.1.1',
     description='Library for extracting cellar data',
     author='LawTech Lab',
     license='MIT',
