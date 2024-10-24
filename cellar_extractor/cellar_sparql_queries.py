153ecd64d1d27548fc32a745e38012efe62d8d30
shashankmc <shashank.chakravarthy@gmail.com>
2024-10-24 07:29:20 +0200

    Added documentation, removed redundant sparql endpoints, added class initialiser with sparql endpoints

--- cellar_extractor/cellar_sparql_queries.py
+++ cellar_extractor/cellar_sparql_queries.py
@@ -1,17 +1,30 @@
 from SPARQLWrapper import JSON, SPARQLWrapper
 from bs4 import BeautifulSoup
 
+
 class CellarSparqlQuery:
 
-    def get_endorsements(self,number):
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        
-        sparql.setQuery(f"""
+    def __init__(self):
+        # Set the SPARQL endpoint
+        _url = 'https://publications.europa.eu/webapi/rdf/sparql'
+        self.sparql = SPARQLWrapper(_url)
+        self.sparql.setReturnFormat(JSON)
+
+    def get_endorsements(self, number):
+        """
+        Retrieves endorsements for a given document number from a SPARQL
+        endpoint.
+        Args:
+            number (str): The document number to query endorsements for.
+        Returns:
+            str: A string containing the endorsements text, parsed from HTML.
+        """
+
+        self.sparql.setQuery(f"""
         PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         SELECT ?Endorsements
-        WHERE {{ 
+        WHERE {{
             ?doc cdm:manifestation_case-law_endorsements ?Endorsements ;
                 owl:sameAs ?w .
             FILTER (?w = <http://publications.europa.eu/resource/celex/{number}.ENG.txt>)
@@ -19,23 +32,27 @@ class CellarSparqlQuery:
         LIMIT 100
         """)
 
-        ret = sparql.queryAndConvert()
-
+        ret = self.sparql.queryAndConvert()
+        print(ret)
         endorsements = ""
         for result in ret['results']['bindings']:
-            endorsements+=result['Endorsements']['value']
+            endorsements += result['Endorsements']['value']
 
-        return BeautifulSoup(endorsements,'html.parser').text
-    
-    def get_grounds(self,number):
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        
-        sparql.setQuery(f"""
+        return BeautifulSoup(endorsements, 'html.parser').text
+
+    def get_grounds(self, number):
+        """
+        Retrieves the grounds of a case-law document based on its CELEX number.
+        Args:
+            number (str): The CELEX number of the case-law document.
+        Returns:
+            str: The grounds of the case-law document as plain text.
+        """
+        self.sparql.setQuery(f"""
         PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         SELECT ?Grounds
-        WHERE {{ 
+        WHERE {{
             ?wc cdm:manifestation_case-law_grounds ?Grounds ;
                 owl:sameAs ?w .
             FILTER (?w = <http://publications.europa.eu/resource/celex/{number}.ENG.txt>)
@@ -43,22 +60,33 @@ class CellarSparqlQuery:
         LIMIT 100
         """)
 
-        ret = sparql.queryAndConvert()
+        ret = self.sparql.queryAndConvert()
 
         grounds = ""
         for result in ret['results']['bindings']:
-            grounds+=result['Grounds']['value']
+            grounds += result['Grounds']['value']
 
-        return BeautifulSoup(grounds,'html.parser').text
-    def get_keywords(self,number):
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        
-        sparql.setQuery(f"""
+        return BeautifulSoup(grounds, 'html.parser').text
+
+    def get_keywords(self, number):
+        """
+        Retrieves keywords for a given CELEX number from the EU Publications
+        SPARQL endpoint.
+        Args:
+            number (str): The CELEX number of the document to retrieve
+            keywords for.
+        Returns:
+            str: A string containing the keywords associated with the given
+            CELEX number, parsed from HTML.
+        Raises:
+            Exception: If there is an issue with the SPARQL query or the
+            endpoint.
+        """
+        self.sparql.setQuery(f"""
         PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         SELECT ?Keywords
-        WHERE {{ 
+        WHERE {{
             ?wc cdm:manifestation_case-law_keywords ?Keywords ;
                 owl:sameAs ?w .
             FILTER (?w = <http://publications.europa.eu/resource/celex/{number}.ENG.txt>)
@@ -66,23 +94,32 @@ class CellarSparqlQuery:
         LIMIT 100
         """)
 
-        ret = sparql.queryAndConvert()
+        ret = self.sparql.queryAndConvert()
 
         keywords = ""
         for result in ret['results']['bindings']:
-            keywords+=result['Keywords']['value']
+            keywords += result['Keywords']['value']
 
-        return BeautifulSoup(keywords,'html.parser').text
+        return BeautifulSoup(keywords, 'html.parser').text
 
-    def get_parties(self,number):
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        
-        sparql.setQuery(f"""
+    def get_parties(self, number):
+        """
+        Retrieves the parties involved in a case-law document
+        from the EU Publications Office SPARQL endpoint.
+
+        Args:
+            number (str): The CELEX number of the case-law document.
+
+        Returns:
+            str: A string containing the parties involved in the
+            case-law document, with HTML tags removed.
+        """
+
+        self.sparql.setQuery(f"""
         PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         SELECT ?Parties
-        WHERE {{ 
+        WHERE {{
             ?wc cdm:manifestation_case-law_parties ?Parties ;
                 owl:sameAs ?w .
             FILTER (?w = <http://publications.europa.eu/resource/celex/{number}.ENG.txt>)
@@ -90,21 +127,30 @@ class CellarSparqlQuery:
         LIMIT 100
         """)
 
-        ret = sparql.queryAndConvert()
+        ret = self.sparql.queryAndConvert()
         parties = ""
         for result in ret['results']['bindings']:
-            parties+=result['Parties']['value']
+            parties += result['Parties']['value']
 
-        return BeautifulSoup(parties,'html.parser').text
-    def get_subjects(self,number):
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        
-        sparql.setQuery(f"""
+        return BeautifulSoup(parties, 'html.parser').text
+
+    def get_subjects(self, number):
+        """
+        Retrieves the subjects associated with a given CELEX
+        number from the SPARQL endpoint.
+
+        Args:
+            number (str): The CELEX number of the document to query.
+
+        Returns:
+            str: A string containing the subjects related to the
+            given CELEX number, parsed from HTML.
+        """
+        self.sparql.setQuery(f"""
         PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         SELECT ?Subjects
-        WHERE {{ 
+        WHERE {{
             ?wc cdm:manifestation_case-law_subject ?Subjects ;
                 owl:sameAs ?w .
             FILTER (?w = <http://publications.europa.eu/resource/celex/{number}.ENG.txt>)
@@ -112,30 +158,28 @@ class CellarSparqlQuery:
         LIMIT 100
         """)
 
-        ret = sparql.queryAndConvert()
+        ret = self.sparql.queryAndConvert()
 
         subjects = ""
         for result in ret['results']['bindings']:
-            subjects+=result['Subjects']['value']
+            subjects += result['Subjects']['value']
 
-        return BeautifulSoup(subjects,'html.parser').text
-    
-    def get_citations(self,source_celex, cites_depth=1, cited_depth=1):
+        return BeautifulSoup(subjects, 'html.parser').text
+
+    def get_citations(self, source_celex, cites_depth=1, cited_depth=1):
         """
-        Gets all the citations one to X steps away. Hops can be specified as either
-        the source document citing another (defined by `cites_depth`) or another document
-        citing it (`cited_depth`). Any numbers higher than 1 denote that new source document
-        citing a document of its own.
-    
-        This specific implementation does not care about intermediate steps, it simply finds
-        anything X or fewer hops away without linking those together.
-        """    
-        sparql = SPARQLWrapper('https://publications.europa.eu/webapi/rdf/sparql')
-        sparql.setReturnFormat(JSON)
-        sparql.setQuery('''
+        Gets all the citations one to X steps away. Hops can be specified
+        as either the source document citing another (defined by `cites_depth`)
+        or another document citing it (`cited_depth`). Any numbers higher than
+        1 denote that new source document citing a document of its own.
+        This specific implementation does not care about intermediate steps
+        it simply finds anything X or fewer hops away without linking those
+        together.
+        """
+        self.sparql.setQuery('''
             prefix cdm: <http://publications.europa.eu/ontology/cdm#>
             prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-    
+
             SELECT DISTINCT * WHERE
             {
             {
@@ -152,12 +196,12 @@ class CellarSparqlQuery:
                 }
             }
             }''' % (source_celex, cites_depth, source_celex, cited_depth))
-        ret = sparql.queryAndConvert()
-    
+        ret = self.sparql.queryAndConvert()
+
         targets = set()
         for bind in ret['results']['bindings']:
             target = bind['name2']['value']
             targets.add(target)
         targets = list(set([el for el in list(targets)]))
-            
+
         return targets
