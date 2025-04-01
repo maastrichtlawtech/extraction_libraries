d5b9b86131adc22d351c52eddcbc8f066c9fe0ac
shashankmc <3445114+shashankmc@users.noreply.github.com>
2025-04-01 16:23:27 +0200

    Updated init file to import modules

--- rechtspraak_extractor/__init__.py
+++ rechtspraak_extractor/__init__.py
@@ -1,4 +1,4 @@
-from rechtspraak_extractor.rechtspraak import get_rechtspraak
-from rechtspraak_extractor.rechtspraak_metadata import get_rechtspraak_metadata
+from rechtspraak_extractor import rechtspraak
+from rechtspraak_extractor import rechtspraak_metadata
 import logging
 logging.basicConfig(level=logging.INFO)
