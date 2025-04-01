30ea7f96ac145acf225eed8e93f5cbd554505a7a
shashankmc <3445114+shashankmc@users.noreply.github.com>
2025-04-01 13:21:19 +0200

    Test file for citations_extractor

--- RS_citations/rechtspraak_citations_extractor/testing.py
+++ RS_citations/rechtspraak_citations_extractor/testing.py
@@ -1,10 +1,15 @@
 import pandas as pd
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from citations_extractor import get_citations
 
 if __name__ == '__main__':
-    name = 'rechtspraak_2018-01-01_2023-06-02_17-45-29_metadata.csv'
+    name = '../../data/rechtspraak_1900-01-01_2024-01-01_11-50-58.csv'
     data = pd.read_csv(name)
-    df = get_citations(data,'','',2)
-    b=2
+    username = os.getenv('LIDO-USERNAME')
+    password = os.getenv('LIDO-PASSWORD')
+    print(username, password)
+    df = get_citations(data,username=username,password=password,threads=2)
+    df.to_csv('../../data/citations.csv',index=False)
