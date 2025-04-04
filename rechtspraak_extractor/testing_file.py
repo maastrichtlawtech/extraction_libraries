5225298f723d570313401624709d1b17cc1f997b
shashankmc <3445114+shashankmc@users.noreply.github.com>
2025-04-04 14:26:43 +0200

    Additional argument for metadata extraction

--- rechtspraak_extractor/testing_file.py
+++ rechtspraak_extractor/testing_file.py
@@ -1,9 +1,11 @@
-from rechtspraak_extractor import get_rechtspraak
+from rechtspraak_extractor.rechtspraak import get_rechtspraak
 from rechtspraak_extractor.rechtspraak_metadata import get_rechtspraak_metadata
 df = get_rechtspraak(sd='2025-01-01',
                      ed='2025-01-10',
                      save_file='y',
                      max_ecli=1000)
-df_2 = get_rechtspraak_metadata(save_file='y', dataframe=df)
-b = 2
+df_2 = get_rechtspraak_metadata(save_file='y',
+                                dataframe=df,
+                                _fake_headers=True)
+# b = 2
 pass
