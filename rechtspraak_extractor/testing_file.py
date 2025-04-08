f0d20ad67d2452ce8750d1d0580b2241388c5951
shashankmc <3445114+shashankmc@users.noreply.github.com>
2025-04-08 15:43:24 +0200

    Updated test file to cater to new changes

--- rechtspraak_extractor/testing_file.py
+++ rechtspraak_extractor/testing_file.py
@@ -2,10 +2,12 @@ from rechtspraak_extractor.rechtspraak import get_rechtspraak
 from rechtspraak_extractor.rechtspraak_metadata import get_rechtspraak_metadata
 df = get_rechtspraak(sd='2025-01-01',
                      ed='2025-01-10',
-                     save_file='y',
+                     save_file='n',
                      max_ecli=1000)
-df_2 = get_rechtspraak_metadata(save_file='y',
+df_2 = get_rechtspraak_metadata(save_file='n',
                                 dataframe=df,
                                 _fake_headers=True)
+print(len(df_2), len(df))
+df_2.to_csv('data/test.csv', index=False, encoding='utf8')
 # b = 2
 pass
